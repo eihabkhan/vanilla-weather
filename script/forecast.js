02e6a8ab7be8cb5fbda47db7959c7a6b9c114c90
Eihab <mastereihab@gmail.com>
2020-08-25 17:08:33 +0100

    added API requests to get city and weather

--- script/forecast.js
+++ script/forecast.js
@@ -1,13 +1,30 @@
 const API_KEY = "WTTWzrIOQx3iSkJsX2d1NCYRXdVVcOVi";
 
+// Get city information
 const getCity = async (city) => {
     const base = "http://dataservice.accuweather.com/locations/v1/cities/search"
     const query = `?apikey=${API_KEY}&q=${city}`
-    const res = await fetch(base + query);
-    const data = await res.json();
-    return data[0];
+    
+    // const res = await fetch(base + query);
+    // const data = await res.json();
+    // return data[0]
+    
+    return fetch (base + query)
+        .then(res => res.json())
+            .then(data => data[0])  
+}
+
+// Get weather information
+const getWeather = async (key) => {
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/`
+    const query = `${key}?apikey=${API_KEY}`
+
+    return fetch(base + query)
+        .then(res => res.json())
+            .then(data => data)
 }
 
 getCity("Frankfurt")
-    .then(data => console.log(data))
+    .then(data => getWeather(data.Key))
+        .then(data => console.log(data))
         .catch(err => console.log(err));
